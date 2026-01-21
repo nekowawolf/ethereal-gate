bb15c239b13fa2b9863713843d77bdd360aa6280
nekowawolf <irukainu20@gmail.com>
2026-01-21 21:34:01 +0700

    feat: add sound game

--- Main_Menu.java
+++ Main_Menu.java
@@ -1,18 +1,27 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class main_menu here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Main_Menu extends World
 {
+    private GreenfootSound bgm;
+
     public Main_Menu()
     {    
         super(1200, 675, 1);
 
+        bgm = new GreenfootSound("bgm.mp3");
+        bgm.setVolume(50);
+
         btnStart startButton = new btnStart();
         addObject(startButton, getWidth() / 2, getHeight() / 2 + 100);
+    }
+
+    public void started()
+    {
+        bgm.playLoop();
+    }
+
+    public void stopped()
+    {
+        bgm.stop();
     }
 }
